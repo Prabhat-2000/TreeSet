26afb40a0518b6d64ade7423b42f7897e77718c4
Prabhat Srivastava <102291706+Prabhat-2000@users.noreply.github.com>
2022-08-15 23:53:16 +0530

    Add files via upload

---
+++ TreeSetDemo.java
@@ -0,0 +1,14 @@
+package dailywork;
+
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<String> Treeset = new TreeSet<String>();
+        Treeset.add("A");
+        Treeset.add("B");
+        Treeset.add("c");
+    }
+}
+
